daa9d99bd8b10a94966b54eb0588251399b7bd18
Xing Xing <mikespook@gmail.com>
2017-05-22 16:04:53 +1200

    upgrade to new gleam standard

--- scripts/init.lua
+++ scripts/init.lua
@@ -10,21 +10,26 @@ Logf("Initialisation")
 
 config.MQTT = {
 	{
-		Addr = "tcp://api.ghub.nz:8883",
+		Addr = RISK.MQTT.Addr,
 		Username = RISK.MQTT.Username,
 		Password = RISK.MQTT.Password,
 	}
 }
 config.Prefix = "iot"
 config.ClientId = "hub"
+
 config.Tasks = {}
 -- Topic: iot/rfid
 config.Tasks["rfid"] = 0
--- TODO
+
 config.Schedule.Tick = 1000
 config.Schedule.Tasks = {}
-config.Schedule.Tasks["task1"] = 5000
 
-function DefaultPublishHandler(c,m) 
-	Log(m.Payload)
+
+function MQTTDefaultHandler(Client, Message) 
+	Logf("Default: %s", Message.Payload)
+end
+
+function ScheduleDefaultFunc(ctx)
+	Log("Scheduler")
 end
--- scripts/rfid.lua
+++ scripts/rfid.lua
@@ -1,11 +1,14 @@
 -- {"uid":"","topic":"","token":""}
+Logf("RFID tag on: %s", Message.Payload)
 local payload = json.decode(Message.Payload)
 data, err = getContactByExtID(payload.uid)
 local rslt = "0"
 if err ~= nil then
 	Logf("Error(%s+%s): %s", Message.Topic, Message.MessageID, err)
 end
-token = Client:Publish(payload.topic, 0, false, payload.token)
-if token:Wait() and token:Error() ~= nil then
-	Log(token.Error())	
+if data ~= nil then
+	token = Client:Publish(payload.topic, 0, false, payload.token)
+	if token:Wait() and token:Error() ~= nil then
+		Log(token.Error())	
+	end
 end
